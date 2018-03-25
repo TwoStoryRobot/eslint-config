18fef4a1be07b79fa1a81fe1df8bcf41be38e898
Jonathan Bowers <jonotron@gmail.com>
2018-03-24 19:17:34 -0700

    disable react/display-name

--- eslint.js
+++ eslint.js
@@ -23,6 +23,7 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "never"],
-    "no-console": "off"
+    "no-console": "off",
+    "react/display-name": "off"
   }
 }
