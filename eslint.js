98025798c80526c2377e78be92e062005a6f4e52
Jonathan Bowers <jonotron@gmail.com>
2018-04-05 17:42:40 -0700

    extend base

--- eslint.js
+++ eslint.js
@@ -1,29 +1,16 @@
 module.exports = {
+  extends: ['base', 'plugin:react/recommended'],
   env: {
-    es6: true,
-    node: true,
-    jest: true,
     browser: true
   },
   parserOptions: {
-    ecmaVersion: 2017,
     ecmaFeatures: {
       jsx: true
     },
     sourceType: 'module'
   },
-  plugins: ['jest', 'react'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:react/recommended'
-  ],
+  plugins: ['react'],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'no-console': 'off',
     'react/display-name': 'off'
   }
 }
